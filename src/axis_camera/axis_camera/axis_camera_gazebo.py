f2c529f4f8aae68d2b9e46f98d2b10b3d8e34105
Lorenzo Bianchi <lnz.bnc@gmail.com>
2024-09-10 15:48:04 +0200

    feat: update axis_camera_gazebo.py

--- src/axis_camera/axis_camera/axis_camera_gazebo.py
+++ src/axis_camera/axis_camera/axis_camera_gazebo.py
@@ -5,7 +5,7 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy, qos_profile_sensor_data
 
-from axis_camera_interfaces.msg import ImagePT, ImageError
+from axis_camera_interfaces.msg import ImagePTZF, PTZF
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import Image, JointState
 
@@ -13,77 +13,6 @@ from threading import Thread, Lock
 import time
 
 from math import pi
-
-class AxisController(Node):
-
-    def __init__(self, axis_camera):
-        super().__init__('axis_controller')
-
-        self.group = MutuallyExclusiveCallbackGroup()
-
-        self.qos_profile = QoSProfile(depth = 0)
-        self.qos_profile.history = QoSHistoryPolicy.KEEP_LAST
-        self.qos_profile.reliability = QoSReliabilityPolicy.BEST_EFFORT
-
-        self.subscriber = self.create_subscription(
-            ImageError,
-            'axis_errors',
-            self.listener_callback,
-            0,
-            callback_group=self.group)
-        self.subscriber  # prevent unused variable warning
-
-        self.axis_camera = axis_camera
-
-        self.publisher_errors_ = self.create_publisher(Float64MultiArray,
-                                                       '/velocity_controller/commands',
-                                                       self.qos_profile,
-                                                       callback_group=self.group)
-
-        self.time_to_wait = 5
-        self.kill_thread = False
-        self.timeout_counter = 0
-        self.timeout_lock = Lock()
-        self.sgn = 1
-
-    def listener_callback(self, msg):
-        #joint_names = ["chassis_to_pan", "pan_to_tilt"]
-        with self.timeout_lock:
-            self.timeout_counter += 1
-
-        cmds = Float64MultiArray()
-        kp = 0.3
-        cmds.data = [kp*msg.ex, kp*msg.ey]
-
-        self.publisher_errors_.publish(cmds)
-
-    def timeout(self):
-        while not self.kill_thread:
-            time.sleep(self.time_to_wait)
-            with self.timeout_lock:
-                if self.timeout_counter > 0:
-                    self.timeout_counter = 0
-                    self.time_to_wait = 5
-                else:
-
-                    with self.axis_camera._lock:
-                        pan = self.axis_camera.pan
-                        tilt = self.axis_camera.tilt
-
-                    if pan > 20*pi/180:
-                        self.sgn = -1
-                    elif pan < -20*pi/180:
-                        self.sgn = 1
-
-                    tilt_des = 35*pi/180
-
-                    cmds = Float64MultiArray()
-                    cmds.data = [self.sgn*0.1, 0.3*(tilt_des-tilt)]
-
-                    self.publisher_errors_.publish(cmds)
-
-                    self.time_to_wait = 1
-
 
 class AxisCamera(Node):
 
@@ -98,16 +27,16 @@ class AxisCamera(Node):
         self.qos_profile.durability = QoSDurabilityPolicy.VOLATILE
 
         self.subscriber_gazebo_image_ = self.create_subscription(Image,
-                                                                 '/axis_camera/image_raw',
+                                                                 '/axis_camera/camera/image_raw',
                                                                  self.listener_callback_image,
                                                                  self.qos_profile,
                                                                  callback_group=self.group)
         self.subscriber_gazebo_image_  # prevent unused variable warning
 
-        self.publisher_imagePT_ = self.create_publisher(ImagePT,
-                                                      'axis_image',
-                                                      self.qos_profile,
-                                                      callback_group=self.group)
+        self.publisher_imagePTZF_ = self.create_publisher(ImagePTZF,
+                                                          'axis_image',
+                                                          self.qos_profile,
+                                                          callback_group=self.group)
 
         self.publisher_image_ = self.create_publisher(
             Image,
@@ -125,18 +54,48 @@ class AxisCamera(Node):
                                                                  callback_group=self.group)
         self.subscriber_gazebo_state_  # prevent unused variable warning
 
+        self.subscriber_command_ = self.create_subscription(
+            PTZF,
+            '/ptz/command',
+            self.listener_callback_command,
+            0,
+            callback_group=self.group)
+        self.subscriber_command_  # prevent unused variable warning
+
+        self.publisher_errors_ = self.create_publisher(Float64MultiArray,
+                                                       '/position_controller/commands',
+                                                       self.qos_profile)
+
         self.get_logger().info("[INFO] starting video stream...")
 
-    def listener_callback_image(self, msg_image):
-        msg = ImagePT()
+    def listener_callback_command(self, msg):
+        with self._lock:
+            curr_pan = self.pan
+            curr_tilt = self.tilt
 
+        if msg.pan_global:
+            pan = msg.pan
+        else:
+            pan = curr_pan + msg.pan
+        if msg.tilt_global:
+            tilt = msg.tilt
+        else:
+            tilt = curr_tilt + msg.tilt
+
+        cmds = Float64MultiArray()
+        cmds.data = [pan, -tilt]
+
+        self.publisher_errors_.publish(cmds)
+
+    def listener_callback_image(self, msg_image):
+        print(1)
+        msg = ImagePTZF()
         msg.image = msg_image
         with self._lock:
-            msg.pan = self.pan
-            msg.tilt = self.tilt
+            msg.ptzf.pan = self.pan
+            msg.ptzf.tilt = self.tilt
 
-        self.publisher_imagePT_.publish(msg)
-
+        self.publisher_imagePTZF_.publish(msg)
         self.publisher_image_.publish(msg_image)
 
     def listener_callback_state(self, msg_state):
@@ -149,25 +108,16 @@ def main(args=None):
     rclpy.init(args=args)
 
     axis_camera = AxisCamera()
-    axis_controller = AxisController(axis_camera)
 
     executor = MultiThreadedExecutor(num_threads=2)
     executor.add_node(axis_camera)
-    executor.add_node(axis_controller)
-
-    timeout_thread = Thread(target=axis_controller.timeout)
-    timeout_thread.start()
 
     executor.spin()
 
     executor.shutdown()
     axis_camera.destroy_node()
-    axis_controller.destroy_node()
 
     axis_camera.destroy_node()
-
-    axis_controller.kill_thread = True
-    timeout_thread.join()
 
     rclpy.shutdown()
 
